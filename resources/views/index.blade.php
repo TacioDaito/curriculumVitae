5e2b032b95afcd809c46780642d44ee1b7854b1e
TacioDaito <taciokikuchi@gmail.com>
2025-03-21 21:03:20 -0300

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
             <section>
                 <h2>Log In</h2>
                 <h3>Already have a account?</h3>
-                <form action="/login" method="post">
+                <form action="{{ route(dashboard) }}" method="post">
                     <input type="text" name="email" placeholder="Type here you email"
                         required>
                     <input type="password" name="password" placeholder="Type here your password"
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::group(['prefix' => 'users'], function () {
+    Route::get('User/dashboard', 'DashboardController@show');
+    Route::get('User/', 'UserController@index');
+    Route::get('User/create', 'UserController@create');
+    Route::post('User/store', 'UserController@store');
+    Route::get('User/edit/{id}', 'UserController@edit');
+    Route::post('User/update/{id}', 'UserController@update');
+    Route::get('User/delete/{id}', 'UserController@delete');
+});
